14a8005783e24aa2a0884c9026c009d9e0dd2c14
James Dizon <dizonjerlin@gmail.com>
2019-05-22 00:25:06 -0700

    Updated Files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,168 +1,193 @@
-const start = document.getElementById("start");
-const quiz = document.getElementById("quiz");
-const question = document.getElementById("question");
-const qImg = document.getElementById("qImg");
-const choiceA = document.getElementById("A");
-const choiceB = document.getElementById("B");
-const choiceC = document.getElementById("C");
-const counter = document.getElementById("counter");
-const timeGauge = document.getElementById("timeGauge");
-const progress = document.getElementById("progress");
-const scoreDiv = document.getElementById("scoreContainer");
+$(document).ready(function(){
+  
+    // event listeners
+    $("#remaining-time").hide();
+    $("#start").on('click', trivia.startGame);
+    $(document).on('click' , '.option', trivia.guessChecker);
+    
+  })
+  
+  var trivia = {
+    // trivia properties
+    correct: 0,
+    incorrect: 0,
+    unanswered: 0,
+    currentSet: 0,
+    timer: 20,
+    timerOn: false,
+    timerId : '',
+    // questions options and answers data
+    questions = {
+        q1: "Who is Goku's best friend?",
+        q2: "Who is Goku's first master?",
+        q3: "What is the name of Goku's brother?",
+        q4: "What is Goku's real name?",
+        q5: "Who is Goku's rival?",
+        q6: "Who is the youngest Super Saiyan?",
+        q7: "Who created the Dragonball radar?",
+        q8: "What is the name of Goku's father?",
+        q9: "What is the name of Goku's cloud?",
+        q10: "What kind of animal does a Saiyan turn into during a full moon?"
+    },
+    options:{
+        q1: ['Bulma', 'Tien', 'Yamcha', 'Krillin'],
+        q2: ['Gohan', 'Roshi', 'Kami', 'Cooler'],
+        q3: ['Raditz', 'Vegeta', 'Luffy', 'ChiChi'],
+        q4: ['Carrot', 'Bardock', 'Dende', 'Kakarot'],
+        q5: ['Vegeta', 'Raditz', 'Goten', 'Gohan'],
+        q6: ['Bulma', 'Krillin', 'Goku', 'Trunks'],
+        q7: ['Gohan', 'Trunks', 'Pan', 'Goten'],
+        q8: ['Piccolo', 'Napa', 'Bardock', 'Yamcha'],
+        q9: ['Stratus', 'Nimbus', 'Nimbostratus', 'Stratocumulus'],
+        q10: ['Dog', 'Cat', 'Ape', 'Dragon']
+    },
+    answers: {
+        q1: 'Krillin',
+        q2: 'Gohan',
+        q3: 'Raditz',
+        q4: 'Kakarot',
+        q5: 'Vegeta',
+        q6: 'Goten',
+        q7: 'Bulma',
+        q8: 'Bardock',
+        q9: 'Nimbus',
+        q10: 'Ape'
+    },
+ // trivia methods
+  // method to initialize game
+  startGame: function(){
+    // restarting game results
+    trivia.currentSet = 0;
+    trivia.correct = 0;
+    trivia.incorrect = 0;
+    trivia.unanswered = 0;
+    clearInterval(trivia.timerId);
+    
+    // show game section
+    $('#game').show();
+    
+    //  empty last results
+    $('#results').html('');
+    
+    // show timer
+    $('#timer').text(trivia.timer);
+    
+    // remove start button
+    $('#start').hide();
 
-let questions = [
-    {
-        question: "Who is Goku's best friend?",
-        choiceA: 'Bulma',
-        choiceB: 'Tien',
-        choiceC: 'Yamcha',
-        choiceD: 'Krillin',
-        correct: "D"
-    },
-    {
-        question: "Who is Goku's first master?",
-        choiceA : 'Gohan',
-        choiceB : 'Roshi',
-        choiceC : 'Kami',
-        choiceD : 'Cooler',
-        correct : "B"
-    },
-    {
-        question: "What is the name of Goku's brother?",
-        choiceA: 'Raditz',  
-        choiceB: 'Vegeta',
-        choiceC: 'Luffy',
-        choiceD: 'ChiChi',
-        correct: 'A'
-    },
-    {
-        question: "What is Goku's real name?",
-        choiceA: 'Carrot',
-        choiceB: 'Bardock',
-        choiceC: 'Dende',
-        choiceD: 'Kakarot',
-        correct: 'D'
-    },
-    {
-        question: "Who is Goku's rival?",
-        choiceA: 'Vegeta',
-        choiceB: 'Raditz',
-        choiceC: 'Goten',
-        choiceD: 'Gohan',
-        correct: 'A'
-    },
-    {
-        question: "Who is the youngest Super Saiyan?",
-        choiceA: 'Gohan',
-        choiceB: 'Trunks',
-        choiceC: 'Pan',
-        choiceD: 'Goten',
-        correct: 'D'
-    },
-    {
-        question: "Who created the Dragonball radar?",
-        choiceA: 'Bulma',
-        choiceB: 'Krillin',
-        choiceC: 'Goku',
-        choiceD: 'Trunks',
-        correct: 'A'
-    },
-    {
-        question: "What is the name of Goku's father?",
-        choiceA: 'Piccolo',
-        choiceB: 'Napa',
-        choiceC: 'Bardock',
-        choiceD: 'Yamcha',
-        correct: 'C'
-    },
-    {
-        question: "What is the name of Goku's cloud?",
-        choiceA: 'Stratus',
-        choiceB: 'Nimbus',
-        choiceC: 'Nimbostratus',
-        choiceD: 'Stratocumulus',
-        correct: 'B'
-    },
-    {
-        question: "What kind of animal does a Saiyan turn into during a full moon?",
-        choiceA: 'Dog',
-        choiceB: 'Cat',
-        choiceC: 'Ape',
-        choiceD: 'Dragon',
-        correct: 'C'
+    $('#remaining-time').show();
+    
+    // ask first question
+    trivia.nextQuestion();
+    
+  },
+  // method to loop through and display questions and options 
+  nextQuestion : function(){
+    
+    // set timer to 20 seconds each question
+    trivia.timer = 10;
+     $('#timer').removeClass('last-seconds');
+    $('#timer').text(trivia.timer);
+    
+    // to prevent timer speed up
+    if(!trivia.timerOn){
+      trivia.timerId = setInterval(trivia.timerRunning, 1000);
     }
-];
-
-let lastQuestionIndex = questions.length - 1;
-let runningQuestionIndex = 0;
-let count = 0;
-const questionTime = 10;
-
-function renderQuestion(){
-    let q = question[runningQuestionIndex];
-    question.innerHTML = "<p>" + q.question + "</p>";
-    choiceA.innerHTML = q.choiceA;
-    choiceB.innerHTML = q.choiceB;
-    choiceC.innerHTML = q.choiceC;
-    choiceD.innerHTML = q.choiceD;
-};
-
-start.addEventListener("click",startQuiz);
-
-function startQuiz(){
-    start.style.display = "none";
-    renderQuestion();
-    quiz.style.display = "block";
-    renderProgress();
-    renderCounter();
-    TIMER = setInterval(renderCounter,1000);
-}
-
-function renderProgress(){
-    for(let qIndex = 0; qIndex <= lastQuestion; qIndex++){
-        progress.innerHTML += "<div class='prog' id="+ qIndex +"></div>";
-    }
-}
-
-function renderCounter(){
-    if(count <= questionTime){
-        counter.innerHTML = count;
-        timeGauge.style.width = count * gaugeUnit + "px";
-        count++
-    }else{
-        count = 0;
-        answerIsWrong();
-        if(runningQuestion < lastQuestion){
-            runningQuestion++;
-            renderQuestion();
-        }else{
-            clearInterval(TIMER);
-            scoreRender();
+    
+    // gets all the questions then indexes the current questions
+    var questionContent = Object.values(trivia.questions)[trivia.currentSet];
+    $('#question').text(questionContent);
+    
+    // an array of all the user options for the current question
+    var questionOptions = Object.values(trivia.options)[trivia.currentSet];
+    
+    // creates all the trivia guess options in the html
+    $.each(questionOptions, function(index, key){
+      $('#options').append($('<button class="option btn btn-info btn-lg">'+key+'</button>'));
+    })
+    
+  },
+  // method to decrement counter and count unanswered if timer runs out
+  timerRunning : function(){
+    // if timer still has time left and there are still questions left to ask
+    if(trivia.timer > -1 && trivia.currentSet < Object.keys(trivia.questions).length){
+      $('#timer').text(trivia.timer);
+      trivia.timer--;
+        if(trivia.timer === 4){
+          $('#timer').addClass('last-seconds');
         }
     }
-}
-
-function checkAnswer(answer){
-    if( answer == questions[runningQuestion].correct){
-        score++;
-        answerIsCorrect();
-    }else{
-        answerIsWrong();
+    // the time has run out and increment unanswered, run result
+    else if(trivia.timer === -1){
+      trivia.unanswered++;
+      trivia.result = false;
+      clearInterval(trivia.timerId);
+      resultId = setTimeout(trivia.guessResult, 1000);
+      $('#results').html('<h3>Out of time! The answer was '+ Object.values(trivia.answers)[trivia.currentSet] +'</h3>');
     }
-    count = 0;
-    if(runningQuestion < lastQuestion){
-        runningQuestion++;
-        renderQuestion();
-    }else{
-        clearInterval(TIMER);
-        scoreRender();
+    // if all the questions have been shown end the game, show results
+    else if(trivia.currentSet === Object.keys(trivia.questions).length){
+      
+      // adds results of game (correct, incorrect, unanswered) to the page
+      $('#results')
+        .html('<h3>Thank you for playing!</h3>'+
+        '<p>Correct: '+ trivia.correct +'</p>'+
+        '<p>Incorrect: '+ trivia.incorrect +'</p>'+
+        '<p>Unaswered: '+ trivia.unanswered +'</p>'+
+        '<p>Please play again!</p>');
+      
+      // hide game sction
+      $('#game').hide();
+      
+      // show start button to begin a new game
+      $('#start').show();
     }
-}
+    
+  },
+  // method to evaluate the option clicked
+  guessChecker : function() {
+    
+    // timer ID for gameResult setTimeout
+    var resultId;
+    
+    // the answer to the current question being asked
+    var currentAnswer = Object.values(trivia.answers)[trivia.currentSet];
+    
+    // if the text of the option picked matches the answer of the current question, increment correct
+    if($(this).text() === currentAnswer){
+      // turn button green for correct
+      $(this).addClass('btn-success').removeClass('btn-info');
+      
+      trivia.correct++;
+      clearInterval(trivia.timerId);
+      resultId = setTimeout(trivia.guessResult, 1000);
+      $('#results').html('<h3>Correct Answer!</h3>');
+    }
+    // else the user picked the wrong option, increment incorrect
+    else{
+      // turn button clicked red for incorrect
+      $(this).addClass('btn-danger').removeClass('btn-info');
+      
+      trivia.incorrect++;
+      clearInterval(trivia.timerId);
+      resultId = setTimeout(trivia.guessResult, 1000);
+      $('#results').html('<h3>Better luck next time! '+ currentAnswer +'</h3>');
+    }
+    
+  },
+  // method to remove previous question results and options
+  guessResult : function(){
+    
+    // increment to next question set
+    trivia.currentSet++;
+    
+    // remove the options and results
+    $('.option').remove();
+    $('#results h3').remove();
+    
+    // begin next question
+    trivia.nextQuestion();
+     
+  }
 
-function answerIsCorrect(){
-    document.getElementById(runningQuestion).style.backgroundColor = "#0f0";
-}
-
-function answerIsWrong(){
-    document.getElementById(runningQuestion).style.backgroundColor = "#f00";
 }
